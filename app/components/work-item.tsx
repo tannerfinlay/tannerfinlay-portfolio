05b9530123c8bc0148c66d2b21b965e66a5e7b81
Tanner Finlay <finlay.matthew19@gmail.com>
2026-09-14 16:17:29 -0600

    Integrating cloudflare wrangler

--- app/components/work-item.tsx
+++ app/components/work-item.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-
 export default function WorkItem({ items }) {
   return (
     <div>
-      {items.map((item) => {
-        return <div className="pt-4">{item}</div>;
+      {items.map((item, index) => {
+        return (
+          <div key={index} className="pt-4">
+            {item}
+          </div>
+        );
       })}
     </div>
   );
