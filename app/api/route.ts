de909d555749af25c1c9b0b5af1895dae4290697
Tanner Finlay <finlay.matthew19@gmail.com>
2026-09-14 19:43:37 -0600

    updating bucket config

--- app/api/route.ts
+++ app/api/route.ts
@@ -3,26 +3,24 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const response = await fetch(
-      "https://tannerfinlay.dev/Finlay_Matthew_Resume.pdf",
-      {
-        cache: "no-store",
-      },
-    );
+    const bucket = process.env.RESUME_BUCKET as any;
 
-    if (!response.ok) {
+    if (!bucket) {
+      return new NextResponse("Bucket missing", { status: 500 });
+    }
+    const response = await bucket.get("Finlay_Matthew_Resume.pdf");
+
+    if (!response) {
       return new NextResponse("Resume file not found", { status: 404 });
     }
 
-    const fileBuffer = await response.arrayBuffer();
+    const headers = new Headers();
+    response.writeHttpMetadata(headers);
+    headers.set("Content-Type", "application/pdf");
+    headers.set("Content-Disposition", 'inline; filename="Resume.pdf"');
+    headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
 
-    return new NextResponse(fileBuffer, {
-      headers: {
-        "Content-Type": "application/pdf",
-        "Content-Disposition":
-          'attachment; filename="Finlay_Matthew_Resume.pdf"',
-      },
-    });
+    return new Response(response.body, { headers });
   } catch (error) {
     return new NextResponse("Internal Server Error", { status: 500 });
   }
